662edda4ee47a5c3d2975fa5663ceb6d52e25e48
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-14 20:54:52 +0500

    useLayoutEffect qo'shildi

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,19 +1,6 @@
 import "./App.css";
-import React, { useState } from "react";
-import Main from "./Main";
-import Alert from "./Alert";
-
-export const AlertContext = React.createContext();
+import React from "react";
 
 export default function App() {
-  const [alert, setAlert] = useState(false);
-  const toggleAlert = () => setAlert((prev) => !prev);
-  return (
-    <AlertContext.Provider value={alert}>
-      <div className="App">
-        <Alert />
-        <Main toggle={toggleAlert} />
-      </div>
-    </AlertContext.Provider>
-  );
+  return <div className="App">Salom</div>;
 }
---
+++ demo/src/useImperativeHandle/useImperativeHandle.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React, { useState, useLayoutEffect, useEffect } from "react";
+
+export default function App() {
+  const [value, setValue] = useState(8);
+  useLayoutEffect(() => {
+    if (value === 0) {
+      setValue(Math.round(Math.random() * 100000));
+    }
+  }, [value]);
+  console.log("Render", value);
+  return <button onClick={() => setValue(0)}>{value}</button>;
+}
